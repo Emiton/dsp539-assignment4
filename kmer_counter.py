ec32ef805466115ddc5693cc96a22fa3118d4bd8
Emiton Alves <emiton_alves@my.uri.edu>
2019-05-11 23:57:44 -0400

    Completed version 1

--- kmer_counter.py
+++ kmer_counter.py
@@ -1,23 +1,57 @@
-import sys
+import matplotlib.pyplot as plt
 import pandas as pd
+import sys
 
 
 def create_dataframe(genome):
+    """
+    Summary line: Creates a dataframe from k-mer data
+
+    Extended description: This function accepts a genomic sequence and creates
+         a dataframe. The dataframe consists of k-mer data extracted from the
+         genome sequence
+
+    Parameters:
+    genome (string): A string of characters representing a genomic sequence
+
+    Return:
+    genome_df: a dataframe which consists of one row for every possible k-mer
+        length from 1 to the length of the genome sequence. Each row will
+        consist of one column of the count of observed k-mers, one column for
+        the count of possible k-mers, and one column which holds a list of
+        strings of the unique k-mers observed
+    """
     genome_length = len(genome)
-    count = 0
     kmer_dict = {}
-    k = 1
 
     for k in range(1, genome_length + 1):
-        print(k)
         mini_dict = count_kmers(genome, k, genome_length)
         kmer_dict[k] = mini_dict
 
     genome_df = pd.DataFrame.from_dict(kmer_dict, orient='index')
-    print(genome_df)
+    return genome_df
 
 
 def count_kmers(genome, k, genome_length):
+    """
+    Summary line: Count all possible and observed k-mers for a given k-mer length
+
+    Extended description: This function will count all possible k-mers and all
+    unique k-mers within a genome sequence for a k-mer length k. It will find
+    and save all unique k-mers
+
+    Parameters:
+    genome (string): a genomic sequence
+    k (int): length of the k-mer sequences being looked for
+    genome_length (int): length of the genomic sequence
+
+    Return:
+    temp_dict: a dictionary which consists of the count of all possible k-mers,
+        the count of all unique k-mers, and a list of strings of all unique
+        k-mers
+
+    """
+    assert(genome_length > 0)
     unique_kmers = []
     count = 0
     temp_dict = {}
@@ -37,19 +71,50 @@ def count_kmers(genome, k, genome_length):
     temp_dict['observed_kmers'] = unique_kmers
     temp_dict['observed_count'] = len(unique_kmers)
     temp_dict['possible_count'] = count
-    print(temp_dict)
     return temp_dict
 
 
-def create_graph():
-    print("Create graph")
+def create_graph(kmer_data):
+    """
+    Summary line: A function which takes a dataframe and creates a graph
 
-def liguistic_proficiency():
-    print("Create graph")
+    Extended description: This function will plot the differences between the
+    observed k-mers and the unique k-mers within a given genomic sequence
 
-# possible kmers
-# unique kmers
-#
+    Parameters:
+    kmer_data (dataframe): A dataframe consisting of all k-mer information for
+        all lengths k from 1 to the length of the genomic sequence
+
+    Return:
+    No return value
+    """
+    kmer_graph = kmer_data[['possible_count', 'observed_count']]\
+        .plot(kind='bar', title='Observed kmers vs Possible kmers', legend=True)
+    kmer_graph.set_xlabel("K (kmer length)")
+    kmer_graph.set_ylabel("Count")
+    plt.show()
+
+
+def linguistic_complexity(kmer_data):
+    """
+    Summary line: Determines the linguistic complexity of genomic sequence
+
+    Extended description: This function determines the linguistic complexity of
+        a genome sequence by determining the observed k-mers and the possible
+        k-mers and then calculating the linguistic complexity using these values
+
+    Parameters:
+    kmer_data (dataframe): A dataframe consisting of all k-mer information for
+        all lengths k from 1 to the length of the genomic sequence
+
+    Return:
+    complexity: A floating point value of the linguistic complexity of a language
+    """
+    observed_kmers = kmer_data['observed_count'].sum()
+    possible_kmers = kmer_data['possible_count'].sum()
+    complexity = observed_kmers / possible_kmers
+    return complexity
+
 
 def main():
     # check user input
@@ -62,9 +127,11 @@ def main():
     with open(input_file, 'r') as file:
         genome = file.read().replace('\n', '')
 
-    # print(genome)
-
     # output results
-    create_dataframe(genome)
+    kmer_data = create_dataframe(genome)
+    create_graph(kmer_data)
+    complexity = linguistic_complexity(kmer_data)
+    print(complexity)
+
 
 main()
